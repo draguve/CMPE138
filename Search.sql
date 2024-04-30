fe7a152139366fe1d0204c9013d83cbfefbe1fa8
Draguve <ritwikpatil1998@gmail.com>
2024-04-30 12:56:47 -0700

    Search

--- Search.sql
+++ Search.sql
@@ -3,10 +3,14 @@ SELECT
   platform,
   name,
   dependent_projects_count,
-  dependent_repositories_count
+  dependent_repositories_count,
+  repository_stars_count
 FROM
-  `bigquery-public-data.libraries_io.projects`
+  `bigquery-public-data.libraries_io.projects_with_repository_fields`
 WHERE
-  name LIKE "Express"
+  name LIKE "%express%"
+ORDER BY
+  repository_stars_count 
+  DESC
 LIMIT
-  100
+  10
